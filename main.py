d60876ac50b43d5334ba467746292e81570087f4
Lamiette <102236519+Lamiette@users.noreply.github.com>
2022-04-22 10:27:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import random
 from random import randint
+
+
+class joueur:
+    def __init__(self,prenom,score) -> None:
+        self.prenom = prenom
+        self.score = score
+
+        
 floor = 0
 roof = 101
 nbr_to_guess = randint(floor,roof)
@@ -29,63 +37,84 @@ while True:
         print("-"*27)
 
 
-for x in range(nombre_de_joueur) :
-    liste_joueur.append(str(input("Entrez votre nom : "))) 
+for joueurs in range(nombre_de_joueur) :
+    prenom = input("Entrez votre nom :")
+    score = 0
+    liste_joueur.append(joueur(prenom, score)) 
 
 
-for y in range(nombre_de_bot) :
-    liste_bot = random.sample(prenom_bot, nombre_de_bot)
+for bots in range(nombre_de_bot) :
+    prenom = random.choice(prenom_bot)
+    score = 0
+    liste_bot.append(joueur(prenom, score))
     
     
 
 print("-"*25)
-print("Voici la liste des joueurs :","\n",*liste_joueur, sep="\n")
-print("-"*25)  
-print("Voici la liste des Bots :","\n",*liste_bot, sep='\n')
+print("Voici la liste des joueurs : ","\n")
+for joueur in liste_joueur:
+    print(joueur.prenom)
+
+print("-"*25) 
+print("Voici la liste des Bots ","\n") 
+for joueur in liste_bot:
+    print(joueur.prenom)
 print("-"*25)
 
 
-for z in liste_joueur:
-    y = ""
-    cpt_joueur=0
-    while y != nbr_to_guess:
-        cpt_joueur += 1
-        y=input("Joueur "+ z +" Saisissez un nombre : ")
-        if y == "":
+for joueur in liste_joueur:
+    tentative_joueur = ""
+    joueur.score=0
+    while tentative_joueur != nbr_to_guess:
+        joueur.score += 1
+        tentative_joueur=input("Joueur "+joueur.prenom+" Saisissez un nombre : ")
+        if tentative_joueur == "":
             exit()
-        if int(y) < nbr_to_guess:
+        if int(tentative_joueur) < nbr_to_guess:
             print ("Plus haut")        
-        elif int(y) > nbr_to_guess:
+        elif int(tentative_joueur) > nbr_to_guess:
             print("Plus bas")
         else:
             print("-" * 20)
-            if cpt_joueur < 2:
-                print("Joueur "+z+" l'emporte en",cpt_joueur,"coup")
-            else:
-                print("Joueur "+z+" l'emporte en",cpt_joueur,"coups")
+            print("Partie Finit !")
+            print("-" * 20)
             break
 
-joueur_bot = 0
 
-for x in liste_bot:
-
-    cpt_bot = 0
-    while joueur_bot != nbr_to_guess:
-        cpt_bot += 1
-        if int(joueur_bot) < nbr_to_guess:
+for joueur in liste_bot:
+    tentative_bot = 0
+    joueur.score=0
+    while tentative_bot != nbr_to_guess:
+        joueur.score += 1
+        if int(tentative_bot) < nbr_to_guess:
             print("Plus haut")
-            floor = joueur_bot +1
-            joueur_bot = randint(floor,roof)            
-            print(joueur_bot)
+            floor = tentative_bot +1
+            tentative_bot = randint(floor,roof)            
+            print(tentative_bot)
             
-        else:
+        elif int(tentative_bot) > nbr_to_guess:
             print("Plus bas")
-            roof = joueur_bot -1
-            joueur_bot = randint(floor,roof)
-            print(joueur_bot)
+            roof = tentative_bot -1
+            tentative_bot = randint(floor,roof)
+            print(tentative_bot)
             
-    else :
-         print("Le bot "+x+ " l'emporte en ",cpt_bot," coups")
-         break
+        elif int(tentative_bot) == nbr_to_guess:
+            print("-" * 20)
+            print("Partie Finit !")
+            print("-" * 20)
+            break
 
 
+
+liste_joueur.sort(key=lambda joueur: joueur.score)
+
+print("Le joueur",liste_joueur[0].prenom,"remporte la partie en",liste_joueur[0].score,"tentatives")
+print("Le joueur",liste_joueur[1].prenom,"termine 2ème en",liste_joueur[1].score,"tentatives")
+print("Le joueur",liste_joueur[2].prenom,"termine 3ème en",liste_joueur[2].score,"tentatives")
+
+liste_bot.sort(key=lambda joueur: joueur.score)
+
+print("-" * 20)
+print("Le joueur",liste_bot[0].prenom,"remporte la partie en",liste_bot[0].score,"tentatives")
+print("Le joueur",liste_bot[1].prenom,"termine 2ème en",liste_bot[1].score,"tentatives")
+print("Le joueur",liste_bot[2].prenom,"termine 3ème en",liste_bot[2].score,"tentatives")
